92a5e3d930f4327cf0fe371c7d16959e7269f8d6
Steffeleffe <steffeleffe@gmail.com>
2019-12-08 21:33:14 +0100

    Change quarkus test.

--- src/test/kotlin/org/steffeleffe/ExampleResourceTest.kt
+++ src/test/kotlin/org/steffeleffe/ExampleResourceTest.kt
@@ -3,18 +3,19 @@ package org.steffeleffe
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
 import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.containsString
 import org.junit.jupiter.api.Test
 
 @QuarkusTest
 open class ExampleResourceTest {
 
     @Test
-    fun testHelloEndpoint() {
+    fun testHealthEndpoint() {
         given()
-          .`when`().get("/hello")
+          .`when`().get("/health")
           .then()
              .statusCode(200)
-             .body(`is`("hello"))
+             .body(containsString("UP"))
     }
 
 }
